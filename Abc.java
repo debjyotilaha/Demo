f97f086b80e49ff0f7fe10dd0882896ba8a9dc34
root <root@ip-172-31-19-214.us-east-2.compute.internal>
2020-09-04 06:07:42 +0000

    learning git well

--- Abc.java
+++ Abc.java
@@ -1,6 +1,7 @@
 public class Abc{
 	public static void main(String args[]){
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Thanks for questions");
 	}
 }
 
